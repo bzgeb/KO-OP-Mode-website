637dc9a3ed5a5df56350a49205cc14d9f5fbfd72
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-04-12 12:40:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoOpmode::Application.config.secret_token = 'c80fe13d09c4b32eb52a599a9f7815401ce3caa3b3c814725363ee0b877cf7e8ed33cea973a8ee19ebbfdda41f32b8fb7f3e0cdb939e6cb18169f190c45d4372'
