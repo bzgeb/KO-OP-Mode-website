567f56d17163802a28b869b692f76b37df4b0dec
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-04-20 17:25:15 -0400

    Fixed mailchimp bug.

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -4,9 +4,9 @@ class MailingListController < ApplicationController
         @lists = gb.lists
         @r = gb.list_subscribe({:id => ENV['MC_NEWSLETTER_ID'], 
                                 :email_address => params['email'],
-                                :merge_var => {:EMAIL => params['email'],
-                                               :FNAME => params['first_name'],
-                                               :LNAME => params['last_name']}})
+                                :merge_vars => {:EMAIL => params['email'],
+                                                :FNAME => params['first_name'],
+                                                :LNAME => params['last_name']}})
         @header = "Failed!"
         @header = "Subscribed!" if @r == true
     end
